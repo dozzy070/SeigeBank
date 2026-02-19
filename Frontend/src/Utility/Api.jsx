e5018d795815038daab84668652a46853bbad28d
dozzy070 <dozzydivinec@gmail.com>
2026-02-19 12:25:30 +0100

    Change API base URL to production server

--- Frontend/src/Utility/Api.jsx
+++ Frontend/src/Utility/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://newwork-1-ubb2.onrender.com/api",
   withCredentials: true,
 });
 
