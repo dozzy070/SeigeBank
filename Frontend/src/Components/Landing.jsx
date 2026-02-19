9d6d900c3edaaf040824f082d0e7bdac00cf86bd
dozzy <dozzydivinec@gmail.com>
2026-02-19 16:08:43 +0100

    Update Landing image paths to match renamed assets

--- Frontend/src/Components/Landing.jsx
+++ Frontend/src/Components/Landing.jsx
@@ -114,8 +114,8 @@ export default function Landing() {
 
       {/* Bank Images */}
       <div className="view-images" aria-hidden>
-        <img src="/assets/seige card.png" className="bank-image img-1" alt="banking illustration 1" />
-        <img src="/assets/seige card2.jpg" className="bank-image img-2" alt="banking illustration 2" />
+        <img src="/assets/seige-card.png" className="bank-image img-1" alt="banking illustration 1" />
+        <img src="/assets/seige-card2.jpg" className="bank-image img-2" alt="banking illustration 2" />
       </div>
     </div>
   </div>
@@ -188,3 +188,4 @@ export default function Landing() {
         </div>
     );
 }
+
